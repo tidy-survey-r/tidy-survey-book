615f20e0d58033caaba55d986bd8d01ec75ed4e3
Stephanie Zimmer <stephaniezimmer@gmail.com>
2023-03-05 12:59:36 -0500

    RECS 2015 and 2020 analysis data

---
+++ DataCleaningScripts/00_Run.R
@@ -0,0 +1,7 @@
+rmarkdown::render(
+  input=here::here("DataCleaningScripts", "RECS_2015_DataPrep.Rmd")
+)
+
+rmarkdown::render(
+  input=here::here("DataCleaningScripts", "RECS_2020_DataPrep.Rmd")
+)
