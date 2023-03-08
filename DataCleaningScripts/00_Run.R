9053698329f0b750411553fde2f13d3adb6ae27a
Stephanie Zimmer <stephaniezimmer@gmail.com>
2023-03-07 19:46:20 -0500

    Adding ANES data

--- DataCleaningScripts/00_Run.R
+++ DataCleaningScripts/00_Run.R
@@ -5,3 +5,7 @@ rmarkdown::render(
 rmarkdown::render(
   input=here::here("DataCleaningScripts", "RECS_2020_DataPrep.Rmd")
 )
+
+rmarkdown::render(
+  input=here::here("DataCleaningScripts", "ANES_2020_DataPrep.Rmd")
+)
