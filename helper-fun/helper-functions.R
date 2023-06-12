8df06696fd1f9bb28ff4db319a840ee784013911
Isabella Velasquez <ivelasq@gmail.com>
2023-06-12 09:44:40 -0500

    Update helper function

--- helper-fun/helper-functions.R
+++ helper-fun/helper-functions.R
@@ -1,28 +1,29 @@
-read_rds_tsr <- function(filename){
+read_rds_tsr <- function(filename) {
   #' Downloads file from Tidy Survey Analysis in R
   #' Reads in file
   #' Deletes file from computer
   
   osf_dl_del_later <- !dir.exists("osf_dl")
   
-  if (osf_dl_del_later){
+  if (osf_dl_del_later) {
     osf_dl_del_later <- TRUE
     dir.create("osf_dl")
-  } 
+  }
   
-  dat_det <- osf_retrieve_node("https://osf.io/gzbkn/?view_only=8ca80573293b4e12b7f934a0f742b957") %>%
+  dat_det <-
+    osf_retrieve_node("https://osf.io/gzbkn/?view_only=8ca80573293b4e12b7f934a0f742b957") %>%
     osf_ls_files() %>%
-    filter(name==filename) %>%
-    osf_download(conflicts="overwrite", path="osf_dl")
+    dplyr::filter(name == filename) %>%
+    osf_download(conflicts = "overwrite", path = "osf_dl")
   
   out <- dat_det %>%
-    pull(local_path) %>%
-    read_rds()
+    dplyr::pull(local_path) %>%
+    readr::read_rds()
   
-  if (osf_dl_del_later){
+  if (osf_dl_del_later) {
     unlink("osf_dl", recursive = TRUE)
   } else{
-    unlink(pull(dat_det, local_path))
+    unlink(dplyr::pull(dat_det, local_path))
   }
   
   return(out)
